be77230d9132892070bb254d461e1190a8cab2bd
carloso <carlos.oliveira@me.com.br>
2024-09-27 18:18:46 -0300

    Spring Bean Life Cycle

---
+++ src/main/java/guru/springframwork/sfg_di/services/CustomBeanPostProcessor.java
@@ -0,0 +1,26 @@
+package guru.springframwork.sfg_di.services;
+
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomBeanPostProcessor implements BeanPostProcessor {
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) {
+
+        if(bean instanceof LifeCycleDemoBean){
+            ((LifeCycleDemoBean) bean).beforeInit();
+        }
+
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) {
+        if(bean instanceof LifeCycleDemoBean){
+            ((LifeCycleDemoBean) bean).afterInit();
+        }
+
+        return bean;
+    }
+}
