174d490f7f2e5894d7f3988cadbf675f55750a51
Carlos Oliveira <vini_carlos10@yahoo.com.br>
2025-08-24 20:29:41 -0300

    adding component scan example

--- src/main/java/guru/springframwork/sfg_di/SfgDiApplication.java
+++ src/main/java/guru/springframwork/sfg_di/SfgDiApplication.java
@@ -1,19 +1,21 @@
 package guru.springframwork.sfg_di;
 
-import guru.springframwork.sfg_di.controllers.ConstructorInjectedController;
-import guru.springframwork.sfg_di.controllers.I18nController;
-import guru.springframwork.sfg_di.controllers.MyController;
-import guru.springframwork.sfg_di.controllers.PropertyInjectedController;
-import guru.springframwork.sfg_di.controllers.SetterInjectedController;
+import guru.springframwork.sfg_di.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan({"guru.springframwork.sfg_di","com.springframework.pets"})
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+		PetController petController = ctx.getBean("petController", PetController.class);
+		System.out.println("--- The Best Pet is ---");
+		System.out.println(petController.whichPetIsTheBest());
 
 		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
 		System.out.println(i18nController.hello());
