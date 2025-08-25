dc446da4ae8d0b6f96d387e1cecab115b80b734d
Carlos Oliveira <vini_carlos10@yahoo.com.br>
2025-08-24 21:01:21 -0300

    adding java profile and primary beans configuration example

--- src/main/java/guru/springframwork/sfg_di/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframwork/sfg_di/config/GreetingServiceConfig.java
@@ -1,13 +1,31 @@
 package guru.springframwork.sfg_di.config;
 
-import guru.springframwork.sfg_di.services.ConstructurGreetingService;
-import guru.springframwork.sfg_di.services.PropertyInjectedGreetingService;
-import guru.springframwork.sfg_di.services.SetterInjectedGreetingService;
+import guru.springframwork.sfg_di.services.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 @Configuration
 public class GreetingServiceConfig {
+
+    @Profile({"ES", "default"})
+    @Bean("i18nService")
+    I18nSpanishGreetingService i18nSpanishGreetingService(){
+        return new I18nSpanishGreetingService();
+    }
+
+    @Profile("EN")
+    @Bean
+    I18nEnglishGreetingService i18nService(){
+        return new I18nEnglishGreetingService();
+    }
+    @Primary
+    @Bean
+    PrimaryGreetingService primaryGreetingService(){
+        return new PrimaryGreetingService();
+    }
 
     @Bean
     ConstructurGreetingService constructurGreetingService(){
--- src/main/java/guru/springframwork/sfg_di/services/PrimaryGreetingService.java
+++ src/main/java/guru/springframwork/sfg_di/services/PrimaryGreetingService.java
@@ -3,8 +3,6 @@ package guru.springframwork.sfg_di.services;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
-@Primary
-@Service
 public class PrimaryGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
